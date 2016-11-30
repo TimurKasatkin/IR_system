dd63ed4b672e4995431d849ac82d25694487e82d
Marat Vinokurov <maratux@gmail.com>
2016-11-30 03:58:31 +0300

    Возвращает значения поисковика

--- web/src/main/scala/ru/innopolis/service/SearchEngine.scala
+++ web/src/main/scala/ru/innopolis/service/SearchEngine.scala
@@ -1,20 +1,28 @@
 package ru.innopolis.service
+import java.net.URLEncoder
 
-class SearchEngine {
-  /**
-    * Принимает поисковый запрос, номер страницы и кол-во записей на страницу,
-    * возвращает список результатов и сколько всего страниц.
-    *
-    * @param searchQuery
-    * @param page
-    * @param limit
-    * @return
-    */
+import com.typesafe.scalalogging.Logger
+import org.json4s.jackson.JsonMethods.parse
+import org.json4s.DefaultFormats
+
+object SearchEngine {
+
+  private implicit val formats = DefaultFormats
+  private val logger = Logger("Search Engine")
+
   def search(searchQuery: String, page: Int = 0, limit: Int = 10): (Iterable[HumanSearchResult], Int, Int) = {
-    val resultsNumber = 83;
-    val pages = resultsNumber / limit + (if (resultsNumber % limit > 0) 1 else 0);
-    return (
-      List.fill(limit)(new HumanSearchResult("#", "Hello world. How are you? I'm fine, thank you.", "Hello world. How are you? I'm fine, thank you. Hello world. How are you? I'm fine, thank you.\n    Hello world. How are you? I'm fine, thank you. Hello world. How are you? I'm fine, thank you.\n    Hello world. How are you? I'm fine, thank you. Hello world. How are you? I'm fine, thank you.\n    Hello world. How are you? I'm fine, thank you. Hello world. How are you? I'm fine, thank you.")),
-      resultsNumber, pages) //всего рез-тов
+    val request = "http://localhost:8081/documents?query=" + URLEncoder.encode(searchQuery, "UTF-8") + s"&pageNumber=$page&pageLimit=$limit"
+    val response = parseJSON(scala.io.Source.fromURL(request).mkString)
+    val resultsNumber = response._2
+    val pages = resultsNumber / limit + (if (resultsNumber % limit > 0) 1 else 0)
+    (response._1, resultsNumber, pages)
+  }
+
+  private def parseJSON(json: String) : (List[HumanSearchResult], Int) = {
+    logger.info(json)
+    val _json = parse(json)
+    val results = (_json \ "docs" ).extract[List[HumanSearchResult]]
+    val resNum = (_json \ "totalNumberOfResults").extract[Integer]
+    return (results, resNum)
   }
 }
